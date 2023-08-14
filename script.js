9ed7ba3573bf24183b7e3e63b923da89ae7c6b66
Vidhi <srivastavavidhi29@gmail.com>
2023-08-14 12:22:03 +0530

    Added Regex

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let ul = document.querySelector("ul");
 let i = document.querySelector(".Add_task");
 let b = document.querySelector("button");
+const Vidhi_name = 'Vidhi';
+const pattern = /^[a-z]{3,}$/;
+console.log(pattern.test(Vidhi_name));
 
 b.addEventListener('click', (e) => {
     e.preventDefault();
     const li = document.createElement("li")
-    li.textContent = i.value;
-    i.value = null;
-    ul.prepend(li);    
+    if(pattern.test(i.value)){
+        li.textContent = i.value;
+        i.value = null;
+        ul.prepend(li);  
+    }
+    else{
+        alert("Enter proper list name");
+    }
 })
 
 
@@ -16,3 +24,7 @@ ul.addEventListener('click', e => {
         e.target.remove();
     }
 })
+
+// const Vidhi_name = 'Vidhi';
+// const pattern = /[a-z]{6,}/;
+// console.log(pattern.test(Vidhi_name));
