2409e733e708f10c0aca7e2591e4bd38a6dd4540
Vidhi <srivastavavidhi29@gmail.com>
2023-08-14 08:45:38 +0530

    edit

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let ul = document.querySelector("ul");
 let i = document.querySelector(".Add_task");
 let b = document.querySelector("button");
 
-b.addEventListener('click', () => {
+b.addEventListener('click', (e) => {
+    e.preventDefault();
     const li = document.createElement("li")
     li.textContent = i.value;
     ul.prepend(li);    
 })
 
-// let li = document.querySelectorAll("li");
 
 ul.addEventListener('click', e => {
     if(e.target.tagName === 'LI'){
